f03b8af7001e145f05a36290edc000379ec3af27
karem-morsy <kareem.morsy2014@gmail.com>
2023-04-10 14:56:06 +0200

    new changes

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 function ProductDetails() {
-  const [product , setProduct] =useState()
+  const [product , setProduct] =useState({})
     let {productId} = useParams();
     useEffect(()=>{
       fetch(`http://localhost:3000/products/${productId}`)
-      .then((res)=>res.json)
+      .then((res)=>res.json())
       .then((data)=>setProduct(data))
     },[])
   return (
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -3,8 +3,29 @@ import { Link, NavLink } from 'react-router-dom'
 import UseFetch from '../hooks/UseFetch'
 
 function Products() {
-  const products = UseFetch("http://localhost:3000/products")
-  console.log(products)
+  const [products , setProducts] = useState([])
+  useEffect(()=>{
+    getAllProducts()
+  },[])
+
+  const getAllProducts=()=>{
+    fetch("http://localhost:3000/products")
+    .then((res)=>res.json())
+    .then((data)=>{
+      setProducts(data)
+    })
+  }
+
+  const deleteProduct= (productId) =>{
+    fetch(`http://localhost:3000/products/${productId}`,{
+      method : "DELETE"
+    })
+    .then((res)=>res.json())
+    .then((data)=>{
+      getAllProducts()
+    })
+  }
+
   
   return (
     <>
@@ -33,7 +54,7 @@ function Products() {
                   <td>{product.description.slice(0,50)}</td>
                   <td>{product.price}</td>
                   <td>
-                    <button className='btn btn-danger btn-sm m-1'>Delete</button>
+                    <button className='btn btn-danger btn-sm m-1' onClick={()=>deleteProduct(product.id)}>Delete</button>
                     <Link to={`/products/${product.id}`} className='btn btn-info btn-sm m-1'>View</Link>
                     <button className='btn btn-primary btn-sm m-1'>Edit</button>
                   </td>
